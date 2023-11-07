15856a33f42c705238c46cc4dc1f03424116a54a
MagCost <113210044+MagCost@users.noreply.github.com>
2023-11-07 19:44:28 +0100

    Added axios integration and migration of project to VS Code and hosting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+export default function SearchEngine(props) {
+  let [city, setCity] = useState("");
+  let [submitted, setSubmitted] = useState(false);
+  let [temperature, setTemperature] = useState(null);
+  let [loading, setLoading] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=57821c3b75b60c68ecd1a8d0dd1aa8d3&units=metric`;
+
+    axios.get(url).then((response) => {
+      setTemperature(response.data.main.temp);
+      setSubmitted(true);
+      setLoading(false);
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search Engine</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      {loading && <p>Loading...</p>}
+      {submitted && city && (
+        <h2>
+          It's {Math.round(temperature)}°C in {city}
+        </h2>
+      )}
     </div>
   );
 }
-
-export default App;
